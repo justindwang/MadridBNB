d321b9a6356cfe3ce3a3a576c01e9cc589696fc5
Krikrifreeze <krikrifreeze@gmail.com>
2020-10-18 20:19:16 -0700

    Generated API-Call Service

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'team2345 land';
+  number = [100, 200, 300, 400];
 }
