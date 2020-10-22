3cc0b8b47dbfaa0af54cb291746a5b29de4b4869
Krikrifreeze <krikrifreeze@gmail.com>
2020-10-22 09:20:03 -0700

    FIXED MERGE BUG

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,10 +4,12 @@ from flask_restful import Api
 from .routes import initialize_routes
 from .parser_listings import run_parser
 from app import globals
+from flask_cors import CORS
 
 globals.initialize()
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 app.config.from_object(Config)
 initialize_routes(api)
