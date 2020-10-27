979bb77f233ed25c95e2a4d49d928360090be5de
justindwang <justindwang@yahoo.com>
2020-10-26 22:56:08 -0700

    Added prototype add, delete, and remove API functions

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -4,7 +4,15 @@ from .search import Listings, Reviews, Neighborhoods
 
 def initialize_routes(api):
     api.add_resource(Listings, "/listings")
+    api.add_resource(AddListing, "/listings/add")
+    api.add_resource(RemoveListing, "/listings/remove")
+    api.add_resource(EditListing, "/listings/edit")
+
     api.add_resource(Reviews, "/reviews")
+    api.add_resource(AddReviews, "/reviews/add")
+    api.add_resource(RemoveReviews, "/reviews/remove")
+    api.add_resource(EditReviews, "/reviews/edit")
+    
     api.add_resource(Neighborhoods, "/neighborhoods")
 
 
