9f3f120fbeefa96a1d19f1dedd4810f0f43cd509
justindwang <justindwang@yahoo.com>
2020-12-01 19:44:50 -0800

    Cleaned search.py code and unused files

--- backend/app/globals(copy).py
+++
@@ -1,5 +0,0 @@
-def initialize():
-    global parsed_data
-    parsed_data = []
-    global line_count
-    line_count = 0
--- backend/app/search.py
+++ backend/app/search.py
@@ -165,89 +165,90 @@ class Analytics(Resource):
         # else:
         #     return "No JSON received", 400
 
-class Reviews(Resource):
-    def post(self):
-        if request.is_json:
-            req = request.get_json()
-            #reviews = <PARSER>.search_reviews(req.get(listing_id))
-            #sets 'reviews' variable to the the expected returned array of Review objects that meet search criteria (search_reviews())
-            response = {
-                "reviews": []
-            }
-            # dictionary that holds an array of dictionaries, each containing data of a single review
-            # e.g. access review data "id" of 1st entry - response["reviews"][0]["id"]
+# Below is for future implementation
+# class Reviews(Resource):
+#     def post(self):
+#         if request.is_json:
+#             req = request.get_json()
+#             #reviews = <PARSER>.search_reviews(req.get(listing_id))
+#             #sets 'reviews' variable to the the expected returned array of Review objects that meet search criteria (search_reviews())
+#             response = {
+#                 "reviews": []
+#             }
+#             # dictionary that holds an array of dictionaries, each containing data of a single review
+#             # e.g. access review data "id" of 1st entry - response["reviews"][0]["id"]
 
-            # for x in reviews:
-            #     temp = {
-                    # "listing_id": x.get_listing_id(),
-                    # "id": x.get_id(),  
-                    # "date": x.get_date(),
-                    # "reviewerName": x.get_reviewer_name(),
-                    # "comments": x.get_comments()
-                    # Accessors for each variable stored in a Review Object
-            #     }
-            #     response['reviews'].append(temp)
-            res = make_response(jsonify(response), 200)
-            return res
-        else:
-            return "No JSON received", 400
+#             # for x in reviews:
+#             #     temp = {
+#                     # "listing_id": x.get_listing_id(),
+#                     # "id": x.get_id(),  
+#                     # "date": x.get_date(),
+#                     # "reviewerName": x.get_reviewer_name(),
+#                     # "comments": x.get_comments()
+#                     # Accessors for each variable stored in a Review Object
+#             #     }
+#             #     response['reviews'].append(temp)
+#             res = make_response(jsonify(response), 200)
+#             return res
+#         else:
+#             return "No JSON received", 400
 
-class AddReview(Resource):
-    def post(self):
-        if request.is_json:
-            req = request.get_json()
-            # add_review(req.get('listing_id'), req.get('comment'))
-            # expected add review function that adds review to csv file given passed listing id and comment parameters
-            res = make_response(200)
-            res.headers.add('Access-Control-Allow-Origin', '*')
-            return res
-        else:
-            return "No JSON received", 400
+# class AddReview(Resource):
+#     def post(self):
+#         if request.is_json:
+#             req = request.get_json()
+#             # add_review(req.get('listing_id'), req.get('comment'))
+#             # expected add review function that adds review to csv file given passed listing id and comment parameters
+#             res = make_response(200)
+#             res.headers.add('Access-Control-Allow-Origin', '*')
+#             return res
+#         else:
+#             return "No JSON received", 400
 
-class RemoveReview(Resource):
-    def post(self):
-        if request.is_json:
-            req = request.get_json()
-            # remove_listing(req.get('listing_id'))
-            # expected remove review function that removes a review from csv file given passed listing id
-            res = make_response(200)
-            res.headers.add('Access-Control-Allow-Origin', '*')
-            return res
-        else:
-            return "No JSON received", 400
+# class RemoveReview(Resource):
+#     def post(self):
+#         if request.is_json:
+#             req = request.get_json()
+#             # remove_listing(req.get('listing_id'))
+#             # expected remove review function that removes a review from csv file given passed listing id
+#             res = make_response(200)
+#             res.headers.add('Access-Control-Allow-Origin', '*')
+#             return res
+#         else:
+#             return "No JSON received", 400
 
-class EditReview(Resource):
-    def post(self):
-        if request.is_json:
-            req = request.get_json()
-            # edit_listing(req.get('listing_id'), req.get('listing_id'), req.get('comment'))
-            # expected edit review function that edits a review from csv file given passed parameters
-            res = make_response(200)
-            res.headers.add('Access-Control-Allow-Origin', '*')
-            return res
-        else:
-            return "No JSON received", 400
+# class EditReview(Resource):
+#     def post(self):
+#         if request.is_json:
+#             req = request.get_json()
+#             # edit_listing(req.get('listing_id'), req.get('listing_id'), req.get('comment'))
+#             # expected edit review function that edits a review from csv file given passed parameters
+#             res = make_response(200)
+#             res.headers.add('Access-Control-Allow-Origin', '*')
+#             return res
+#         else:
+#             return "No JSON received", 400
 
-class Neighborhoods(Resource):
-    def post(self):
-        if request.is_json:
-            req = request.get_json()
-            #neighborhoods = <PARSER>.search_neighborhoods(req.get(neighborhoodName), req.get(neighborhoodGroup))
+# class Neighborhoods(Resource):
+#     def post(self):
+#         if request.is_json:
+#             req = request.get_json()
+#             #neighborhoods = <PARSER>.search_neighborhoods(req.get(neighborhoodName), req.get(neighborhoodGroup))
             
-            response = {
-                "neighborhoods": []
-            }
-            # dictionary that holds an array of dictionaries, each containing data of a single neighborhood
-            # e.g. access neighborhood data "neighborhoodGroup" of 1st entry - response["neighborhoods"][0]["neighborhoodGroup"]
+#             response = {
+#                 "neighborhoods": []
+#             }
+#             # dictionary that holds an array of dictionaries, each containing data of a single neighborhood
+#             # e.g. access neighborhood data "neighborhoodGroup" of 1st entry - response["neighborhoods"][0]["neighborhoodGroup"]
 
-            # for x in neighborhoods:
-            #     temp = {
-                    # "neighborhoodGroup": x.get_neighborhood_group(),
-                    # "neighborhood": x.get_neighborhood()
-                    # Accessors for each variable stored in a Neighborhood Object
-            #     }
-            #     response['neighborhoods'].append(temp)
-            res = make_response(jsonify(response), 200)
-            return res
-        else:
-            return "No JSON received", 400
+#             # for x in neighborhoods:
+#             #     temp = {
+#                     # "neighborhoodGroup": x.get_neighborhood_group(),
+#                     # "neighborhood": x.get_neighborhood()
+#                     # Accessors for each variable stored in a Neighborhood Object
+#             #     }
+#             #     response['neighborhoods'].append(temp)
+#             res = make_response(jsonify(response), 200)
+#             return res
+#         else:
+#             return "No JSON received", 400
